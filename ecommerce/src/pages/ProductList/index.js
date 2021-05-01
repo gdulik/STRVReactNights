590162e0e130bc3722eae9cb549fa9623b508542
gdulik <tomas.gdula@gmail.com>
2021-05-02 00:34:02 +0200

    Implemented redux and Cart component

--- ecommerce/src/pages/ProductList/index.js
+++ ecommerce/src/pages/ProductList/index.js
@@ -1,34 +1,64 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-import ProductListComponent from './components/ProductList'
 import Layout from '../../components/Layout'
 import Loader from '../../components/Loader'
 import { H1 } from '../../components/Typography'
 
 import { getProducts } from './../../api/get-products'
+import { addProduct } from './../../store/cartItems/actions'
+import { loadProducts } from './../../store/products/actions'
+import Product from './Product'
+import { ProductsWrap } from './styled'
 
-class ProductList extends Component {
+class Products extends Component {
   constructor(props) {
     super(props)
     this.state = {
       isLoading: true,
-      products: [],
     }
   }
   async componentDidMount() {
-    let products = await getProducts()
-    this.setState({ products, isLoading: false })
+    if (this.props.products.length === 0) {
+      const products = await getProducts()
+      this.props.loadProducts(products)
+    }
+    this.setState({ isLoading: false })
   }
+
+  handleAddToCart = (productId, evt) => {
+    evt.preventDefault()
+    this.props.addProduct(productId)
+  }
+
   render() {
-    const { isLoading, products } = this.state
     return (
       <Layout>
         <H1 textAlign="center">E-Commerce app</H1>
-        {isLoading && <Loader />}
-        {products && <ProductListComponent products={products} />}
+        {this.state.isLoading && <Loader />}
+        <ProductsWrap>
+          {this.props.products.map((product) => (
+            <Product
+              key={product.id}
+              node={product}
+              onAddToCart={this.handleAddToCart}
+            />
+          ))}
+        </ProductsWrap>
       </Layout>
     )
   }
 }
+
+const mapStateToProps = (state) => ({
+  products: state.products,
+})
+
+const mapDispatchToProps = {
+  addProduct,
+  loadProducts,
+}
+
+const ProductList = connect(mapStateToProps, mapDispatchToProps)(Products)
 
 export { ProductList }
